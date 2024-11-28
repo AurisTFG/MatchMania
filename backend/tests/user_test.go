40d6fafdfda1de3a25d2f23f27911bdef9e5180a
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2024-11-28 06:29:26 +0200

    placeholder tests package

---
+++ backend/tests/user_test.go
@@ -0,0 +1,26 @@
+package tests
+
+import (
+	"MatchManiaAPI/models"
+	"testing"
+)
+
+func TestPasswordHashing(t *testing.T) {
+	// Create a new user
+	user := models.User{
+		Email:    "test@gmail.com",
+		Password: "password",
+	}
+
+	if err := user.HashPassword(); err != nil {
+		t.Errorf("Error hashing password")
+	}
+
+	if user.Password == "password" {
+		t.Errorf("Password was not hashed")
+	}
+
+	if !user.ComparePassword("password") {
+		t.Errorf("Password was not compared correctly")
+	}
+}
