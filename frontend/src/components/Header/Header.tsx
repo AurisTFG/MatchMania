0804e45c6a9c3accb8722b5ad77f432cc95ba689
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2024-12-20 14:16:26 +0200

    fix profile menu anchor

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -26,6 +26,7 @@ export default function Header() {
 
   const handleLogout = () => {
     console.log("Logging out...");
+    setAnchorEl(null);
     logout();
     setUser(null);
     navigate("/");
--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,21 +1,13 @@
-import { Button } from "@mui/material";
 import { UseAuth } from "../../components/Auth/AuthContext";
 
 const Profile = () => {
   const { user } = UseAuth();
-
-  const handleEditProfile = () => {
-    console.log("Editing profile...");
-  };
 
   return (
     <div>
       <h1>Profile</h1>
       <p>Username: {user?.username}</p>
       <p>Email: {user?.email}</p>
-      <Button variant="contained" color="primary" onClick={handleEditProfile}>
-        Edit Profile
-      </Button>
     </div>
   );
 };
