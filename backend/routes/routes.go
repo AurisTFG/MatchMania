5f4ee1527d1aa3a081bdc8fb971587247c41e486
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2024-12-20 03:30:11 +0200

    fix routes....

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -21,6 +21,8 @@ func ApplyRoutes(
 	{
 		auth := v1.Group("/auth")
 		{
+			auth.GET("/seasons", c.SeasonController.GetAllSeasons)
+
 			auth.POST("/signup", c.AuthController.AuthSignUp)
 			auth.POST("/login", c.AuthController.AuthLogIn)
 			auth.POST("/logout", c.AuthController.AuthLogOut)
@@ -29,34 +31,34 @@ func ApplyRoutes(
 
 		users := v1.Group("/users")
 		{
-			users.GET("/", c.UserController.GetAllUsers)
-			users.GET("/:userId", c.UserController.GetUserByID)
-			users.PATCH("/:userId", c.AuthMiddleware.RequireAuth, c.UserController.UpdateUser)
+			users.GET("", c.UserController.GetAllUsers)
+			users.GET(":userId", c.UserController.GetUserByID)
+			users.PATCH(":userId", c.AuthMiddleware.RequireAuth, c.UserController.UpdateUser)
 		}
 
 		seasons := v1.Group("/seasons")
 		{
-			seasons.GET("/", c.SeasonController.GetAllSeasons)
-			seasons.GET("/:seasonId", c.SeasonController.GetSeason)
-			seasons.POST("/", c.AuthMiddleware.RequireAuth, c.SeasonController.CreateSeason)
-			seasons.PATCH("/:seasonId", c.AuthMiddleware.RequireAuth, c.SeasonController.UpdateSeason)
-			seasons.DELETE("/:seasonId", c.AuthMiddleware.RequireAuth, c.SeasonController.DeleteSeason)
+			seasons.GET("", c.SeasonController.GetAllSeasons)
+			seasons.GET(":seasonId", c.SeasonController.GetSeason)
+			seasons.POST("", c.AuthMiddleware.RequireAuth, c.SeasonController.CreateSeason)
+			seasons.PATCH(":seasonId", c.AuthMiddleware.RequireAuth, c.SeasonController.UpdateSeason)
+			seasons.DELETE(":seasonId", c.AuthMiddleware.RequireAuth, c.SeasonController.DeleteSeason)
 
 			teams := seasons.Group("/:seasonId/teams")
 			{
-				teams.GET("/", c.TeamController.GetAllTeams)
-				teams.GET("/:teamId", c.TeamController.GetTeam)
-				teams.POST("/", c.AuthMiddleware.RequireAuth, c.TeamController.CreateTeam)
-				teams.PATCH("/:teamId", c.AuthMiddleware.RequireAuth, c.TeamController.UpdateTeam)
-				teams.DELETE("/:teamId", c.AuthMiddleware.RequireAuth, c.TeamController.DeleteTeam)
+				teams.GET("", c.TeamController.GetAllTeams)
+				teams.GET(":teamId", c.TeamController.GetTeam)
+				teams.POST("", c.AuthMiddleware.RequireAuth, c.TeamController.CreateTeam)
+				teams.PATCH(":teamId", c.AuthMiddleware.RequireAuth, c.TeamController.UpdateTeam)
+				teams.DELETE(":teamId", c.AuthMiddleware.RequireAuth, c.TeamController.DeleteTeam)
 
 				results := teams.Group("/:teamId/results")
 				{
-					results.GET("/", c.ResultController.GetAllResults)
-					results.GET("/:resultId", c.ResultController.GetResult)
-					results.POST("/", c.AuthMiddleware.RequireAuth, c.ResultController.CreateResult)
-					results.PATCH("/:resultId", c.AuthMiddleware.RequireAuth, c.ResultController.UpdateResult)
-					results.DELETE("/:resultId", c.AuthMiddleware.RequireAuth, c.ResultController.DeleteResult)
+					results.GET("", c.ResultController.GetAllResults)
+					results.GET(":resultId", c.ResultController.GetResult)
+					results.POST("", c.AuthMiddleware.RequireAuth, c.ResultController.CreateResult)
+					results.PATCH(":resultId", c.AuthMiddleware.RequireAuth, c.ResultController.UpdateResult)
+					results.DELETE(":resultId", c.AuthMiddleware.RequireAuth, c.ResultController.DeleteResult)
 				}
 			}
 		}
