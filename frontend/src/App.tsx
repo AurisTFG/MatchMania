0d9059b622821627d3b158fa723220bedb5e8045
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2025-04-12 19:18:41 +0300

    refactor: simplify App component by introducing Providers and removing unnecessary styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,38 @@
-import { ThemeProvider } from "@mui/material/styles";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Content from "./components/Content/Content.tsx";
-import Footer from "./components/Footer/Footer.tsx";
-import Header from "./components/Header/Header.tsx";
-import { queryClient } from "./configs/queryClient.ts";
+import { Route, Routes } from "react-router-dom";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
-import HomePage from "./pages/Home/HomePage.tsx";
+import HomePage from "./pages/Home/HomePage";
 import NotFound from "./pages/NotFound/NotFound";
-import Profile from "./pages/Profile/Profile.tsx";
-import ResultsPage from "./pages/Seasons/ResultsPage.tsx";
-import SeasonsPage from "./pages/Seasons/SeasonsPage.tsx";
-import TeamsPage from "./pages/Seasons/TeamsPage.tsx";
-import { AuthProvider } from "./providers/AuthProvider.tsx";
-import theme from "./styles/theme.ts";
+import Profile from "./pages/Profile/Profile";
+import ResultsPage from "./pages/Seasons/ResultsPage";
+import SeasonsPage from "./pages/Seasons/SeasonsPage";
+import TeamsPage from "./pages/Seasons/TeamsPage";
+import Providers from "./providers/Providers";
 import "./styles/global.css";
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <BrowserRouter>
-            <Header />
-            <Content>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/seasons" element={<SeasonsPage />} />
-                <Route
-                  path="/seasons/:seasonId/teams"
-                  element={<TeamsPage />}
-                />
-                <Route
-                  path="/seasons/:seasonId/teams/:teamId/results"
-                  element={<ResultsPage />}
-                />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Content>
-            <Footer />
-          </BrowserRouter>
-        </AuthProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </ThemeProvider>
+    <Providers>
+      <Header />
+      <Content>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/seasons" element={<SeasonsPage />} />
+          <Route path="/seasons/:seasonId/teams" element={<TeamsPage />} />
+          <Route
+            path="/seasons/:seasonId/teams/:teamId/results"
+            element={<ResultsPage />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Content>
+      <Footer />
+    </Providers>
   );
 }
