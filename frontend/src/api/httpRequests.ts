5d1665c1a40440d7963694eab64f05355779d525
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2025-04-13 04:21:36 +0300

    refactor: fix types in httpRequests, remove putRequest, make deleteRequest return void

--- frontend/src/api/httpRequests.ts
+++ frontend/src/api/httpRequests.ts
@@ -3,7 +3,7 @@ import axiosClient from '../configs/axiosClient';
 export const getRequest = async <T>({ url }: { url: string }): Promise<T> => {
   const response = await axiosClient.get<T>(url);
 
-  return response;
+  return response as T;
 };
 
 export const postRequest = async <T>({
@@ -15,29 +15,7 @@ export const postRequest = async <T>({
 }): Promise<T> => {
   const response = await axiosClient.post<T>(url, body);
 
-  return response;
-};
-
-export const putRequest = async <T>({
-  url,
-  body,
-}: {
-  url: string;
-  body?: unknown;
-}): Promise<T> => {
-  const response = await axiosClient.put<T>(url, body);
-
-  return response;
-};
-
-export const deleteRequest = async <T>({
-  url,
-}: {
-  url: string;
-}): Promise<T> => {
-  const response = await axiosClient.delete<T>(url);
-
-  return response;
+  return response as T;
 };
 
 export const patchRequest = async <T>({
@@ -49,5 +27,13 @@ export const patchRequest = async <T>({
 }): Promise<T> => {
   const response = await axiosClient.patch<T>(url, body);
 
-  return response;
+  return response as T;
+};
+
+export const deleteRequest = async ({
+  url,
+}: {
+  url: string;
+}): Promise<void> => {
+  await axiosClient.delete(url);
 };
