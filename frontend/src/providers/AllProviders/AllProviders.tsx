29836af26672816fcbf6560c7c48a81120ccc9ee
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2025-04-15 00:20:01 +0300

    implement toast from Sonner

--- frontend/src/providers/AllProviders/AllProviders.tsx
+++ frontend/src/providers/AllProviders/AllProviders.tsx
@@ -2,18 +2,25 @@ import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'sonner';
 import { queryClient } from '../../configs/queryClient';
 import theme from '../../styles/theme';
 import { AuthProvider } from '../AuthProvider';
 
 /* eslint-disable prettier/prettier */
-export default function AllProviders({ children }: { children: React.ReactNode }) {
+export default function AllProviders({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
+          <Toaster
+            richColors
+            visibleToasts={4}
+            position="bottom-right"
+          />
           <BrowserRouter>
             {children}
           </BrowserRouter>
@@ -23,4 +30,3 @@ export default function AllProviders({ children }: { children: React.ReactNode }
     </ThemeProvider>
   );
 }
- 
