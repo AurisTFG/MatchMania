ef75bfb267449f0dc0636c5bda125937802b1575
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2025-04-15 01:09:43 +0300

    chore: remove comment

--- backend/middlewares/gin/error_middleware.go
+++ backend/middlewares/gin/error_middleware.go
@@ -17,7 +17,6 @@ func ErrorMiddleware() gin.HandlerFunc {
 				fmt.Printf("[ERROR] Panic recovered: %v\n%s\n", err, string(stack))
 
 				if gin.Mode() == gin.DebugMode {
-					// TODO: Log the error and stack trace to a file or monitoring system
 					r.InternalServerError(c, "Internal Server Error: "+err.(string))
 				} else {
 					r.InternalServerError(c, "Internal Server Error")
