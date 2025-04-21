946d329db45376c9f1ad4d8e93e7bf69425ea44b
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2025-04-21 22:24:23 +0300

    refactor: remove "Required" validators from the frontend

--- frontend/src/validators/auth/loginDtoValidator.ts
+++ frontend/src/validators/auth/loginDtoValidator.ts
@@ -2,14 +2,10 @@ import { z } from 'zod';
 import { LoginDto } from '../../types/dtos/requests/auth/loginDto';
 
 export const loginDtoValidator = z.object({
-  email: z
-    .string()
-    .min(1, { message: 'Email is required.' })
-    .email({ message: 'Email must be a valid email address.' }),
+  email: z.string().email({ message: 'Email must be a valid email address.' }),
 
   password: z
     .string()
-    .min(1, { message: 'Password is required.' })
     .min(5, { message: 'Password must be at least 5 characters long.' })
     .max(100, { message: 'Password must be at most 100 characters long.' }),
 }) satisfies z.ZodType<LoginDto>;
