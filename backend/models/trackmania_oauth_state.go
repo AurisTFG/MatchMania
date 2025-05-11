8b637c6ce0775f713497f53134831da7ce8606cf
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2025-05-11 20:33:35 +0300

    chore: add missing gorm not null

--- backend/models/trackmania_oauth_state.go
+++ backend/models/trackmania_oauth_state.go
@@ -8,8 +8,8 @@ import (
 
 type TrackmaniaOauthState struct {
 	Id        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	CreatedAt time.Time
-	State     string `gorm:"not null;unique"`
+	CreatedAt time.Time `gorm:"not null"`
+	State     string    `gorm:"not null;unique"`
 
 	UserId uuid.UUID `gorm:"not null;unique"`
 
